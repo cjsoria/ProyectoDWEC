1c48f5104e89d0e8b17b26aff4647f2b67e0e234
Cristian Javier Soria Fajardo <cristianxico@hotmail.com>
2016-10-21 10:26:03 +0200

    ...

--- tresEnRaya/worksheet3_2.js
+++ tresEnRaya/worksheet3_2.js
@@ -1,22 +1,28 @@
 class Table {
 	constructor(){
 		var tab = `<table border="0" cellspacing="2" bgcolor="black" width="300">`;
-		for (var j=0;j<3;j++) {
+		for (var j=0; j<3; j++) {
 			tab += `<tr bgcolor="white" height="100">`;
-			for (var i=0;i<3;i++) {
+			for (var i=0; i<3;i ++) {
 				tab +=`<td id=${j}.${i} width="100"></td>`;
 			}
 			tab += `</tr>`;
 		}
 		tab += `</table>`
 		document.getElementById("table").innerHTML = tab;
+		this.dibujar();
+	}
 
-		for (var a=0;a<3;a++) {
-			for (var b=0;b<3;b++) {
-				document.getElementById("`${a}.${b}`").addEventListener("click", function() {document.getElementById(`${a}.${b}`).innerHTML = "Hello World";
+	dibujar() {
+		for (var a=0; a<3; a++) {
+			for (var b=0; b<3; b++) {
+				document.getElementById(`${a}.${b}`).addEventListener("click", function(e){
+					e.target.innerHTML = "Hello World";
 				});
-			}
-		}		
+				
+			};
+			
+		}
 	}
 }
 
